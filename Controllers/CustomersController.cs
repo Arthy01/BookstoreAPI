9f2aed3cfb5ccde4ddef91248632c725c81f942a
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-11 13:40:11 +0100

    add CustomersController

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookstoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET: api/<CustomersController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (var db = new DatabaseHelper())
+            {
+                string query = "SELECT * FROM customers limit 10";
+                DataTable result = db.ExecuteQuery(query);
+
+                List<object> customers = new List<object>();
+                foreach (DataRow row in result.Rows)
+                {
+                    customers.Add(new
+                    {
+                        Id = row["id"]
+                    });
+                }
+                return Ok(customers);
+            }
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
