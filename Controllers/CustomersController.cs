8d650d17901a4f087e3002737596ffc3bec4b5c5
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-11 14:24:09 +0100

    added all Customer methods

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Mvc;
 using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,9 +22,11 @@ namespace BookstoreAPI.Controllers
                 List<object> customers = new List<object>();
                 foreach (DataRow row in result.Rows)
                 {
+                    // TODO: Atrribute bearbeiten
                     customers.Add(new
                     {
-                        Id = row["id"]
+                        Id = row["id"],
+                        Name = row["name"]
                     });
                 }
                 return Ok(customers);
@@ -32,27 +35,67 @@ namespace BookstoreAPI.Controllers
 
         // GET api/<CustomersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            using (var db = new DatabaseHelper())
+            {
+                string query = $"SELECT * FROM customers WHERE customers.id = {id} limit 10";
+                DataTable result = db.ExecuteQuery(query);
+
+                List<object> customers = new List<object>();
+                foreach (DataRow row in result.Rows)
+                {
+                    // TODO: Atrribute bearbeiten
+                    customers.Add(new
+                    {
+                        Id = row["id"],
+                        Name = row["name"]
+                    });
+                }
+                return Ok(customers);
+            }
         }
 
         // POST api/<CustomersController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            using (var db = new DatabaseHelper())
+            {
+                // TODO: Konvertierung und Standardwerte hinzufügen
+                value = "Hier wird verabeitet";
+
+                string query = $"INSERT INTO customers (name) VALUES ('{value}')";
+                db.ExecuteNonQuery(query);
+
+            }
         }
 
         // PUT api/<CustomersController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            using (var db = new DatabaseHelper())
+            {
+                // TODO: Konvertierung und Standardwerte hinzufügen
+                value = "Hier wird verabeitet";
+
+                string query = $"UPDATE customers SET name = '{value}' WHERE id = {id}";
+                db.ExecuteNonQuery(query);
+
+            }
         }
 
         // DELETE api/<CustomersController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            using (var db = new DatabaseHelper())
+            {
+                string query = $"DELETE FROM customers WHERE id = {id}";
+                db.ExecuteNonQuery(query);
+
+            }
         }
     }
 }
