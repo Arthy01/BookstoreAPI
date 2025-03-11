7745500f0692935dc6b4e684a74b468d554d670d
Arthy01 <philipeveschaefer@gmail.com>
2025-03-11 15:54:46 +0100

    Updated Order Model

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,6 +2,9 @@
 {
     public class Order
     {
-
+        public ulong Id { get; set; }
+        public DateTime Issued { get; set; }
+        public float Amount { get; set; }
+        public ulong CustomerID { get; set; }
     }
 }
